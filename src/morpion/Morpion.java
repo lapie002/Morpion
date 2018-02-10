c9338ec5a72742d5449bccf3db2679b71b19d197
lapie <lapie@192.168.2.220>
2018-02-10 14:17:57 -0500

    commit test premieres methodes

--- src/morpion/Morpion.java
+++ src/morpion/Morpion.java
@@ -19,16 +19,58 @@ public class Morpion {
     public Morpion(int numjoueur){
         
         this.numjoueur = numjoueur;
+        this.fini = false;
+        this.nbCasesVides = 9;
+        
+        //initialise la grille
+        this.grille = new int[3][3];
         
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
                 this.grille[i][j] = 0; 
             }
         }
-        
-        
-        
     }
+    
+    public int[] saisiChoix(){
+        
+        Scanner sc = new Scanner(System.in);
+        int[] coor = new int[2];
+        boolean correct = false; 
+        
+        while(!correct){
+            
+            System.out.println("Entrez une ligne ");
+            int ligne = sc.nextInt();
+            System.out.println("Entrez une Colonne ");
+            int colonne = sc.nextInt();
+            
+            if(ligne>=0 && ligne<3 && colonne>=0 && colonne<3){
+                coor[0] = ligne;
+                coor[1] = colonne;
+                correct = true;
+            }
+        }
+        return coor;
+    }
+    
+    public String toStringGrille(){
+        
+        String str = ""; 
+        
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                str = str + this.grille[i][j] + " ";
+            }
+            str = str + "\n";
+        }
+         
+         return str + "A toi de jouer !!! Joueur numéro: " + this.numjoueur +"\n";
+    }
+    
+    
+    
+    
     
     
     
@@ -53,6 +95,15 @@ public class Morpion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // test initialistation de grille
+        Morpion mp = new Morpion(2);
+        // test methode saisiChoix()
+        mp.saisiChoix();
+        // test methode toStringGrille() sur la grille initialisée
+        System.out.print(mp.toStringGrille());
+        
+        
     }
     
     
